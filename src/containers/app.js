c3b7ce2082487020670d2f19b63bdaeedbd4d97e
ojbruce <olivia.j.bruce@gmail.com>
2018-01-21 21:30:15 +0100

    Update app element

---
+++ src/containers/app.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { addStanza } from './../actions/actions'
+
+import ChatWindow from './chat-window'
+require('./app.scss')
+
+const App = ({stanzas, addStanza}) => (
+  <div className='discuze__main-section'>
+    <ChatWindow
+      stanzas={stanzas}
+      chatName={'Alice'}
+      addStanza={addStanza}
+    />
+    <ChatWindow
+      stanzas={stanzas}
+      chatName={'Bob'}
+      addStanza={addStanza}
+    />
+  </div>
+)
+
+App.propTypes = {
+  stanzas: PropTypes.array.isRequired,
+  addStanza: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) =>
+  ({stanzas: state})
+
+const mapDispatchToProps = (dispatch) =>
+  ({addStanza: bindActionCreators(addStanza, dispatch)})
+
+const populatedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
+
+export default populatedApp
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers/reducers'
 
-import App from './components/App'
+import App from './containers/app'
+require('./style/base.scss')
 
-require('./base.scss')
+const store = createStore(reducer)
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 )
